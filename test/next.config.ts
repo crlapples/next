3e24f4c18699150773bbf27e122a4439db244e08
crlapples <crlapples19@gmail.com>
2025-02-23 20:04:54 +0000

    a

--- test/next.config.ts
+++ test/next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  basePath: "/next",
-  assetPrefix: "/next/",
-  images: {
-    unoptimized: true
-  }
+  output: "export"
 };
 
 export default nextConfig;
